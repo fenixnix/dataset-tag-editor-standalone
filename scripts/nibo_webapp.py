643e7e3cf2d667eba53011c464d8a89311010324
Nibo <57319138@qq.com>
2025-01-23 13:01:52 +0800

    Update nibo_webapp.py

--- scripts/nibo_webapp.py
+++ scripts/nibo_webapp.py
@@ -4,6 +4,7 @@ os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 from pathlib import Path
 
 from PIL import Image
+import gradio as gr
 
 from dataset_tag_editor.tagger import WaifuDiffusion
 
@@ -16,13 +17,12 @@ def get_tags(image,thresh:float = 0.5):
     sorted_keys = [item[0] for item in sorted_items]
     return sorted_keys
 
-image = Image.open("D:/datasets/1.jpg")
-sorted_keys = get_tags(image)
-print(sorted_keys)
+def get_tags_str(image,thresh:float = 0.5):
+    return ",".join(get_tags(image,thresh))
 
-if __name__ == "__main__":
-    root_path = "Z:/comic_database/comic_grid/21世紀小福星"
-    output_fn = "Z:/comic_database/comic_grid/21世紀小福星/tags.txt"
+# root_path = "Z:/comic_database/comic_grid/21世紀小福星"
+# output_fn = "Z:/comic_database/comic_grid/21世紀小福星/tags.txt"
+def batch_tag(get_tags, root_path, output_fn):
     with open(output_fn,'w',encoding="utf-8") as fw:
         for p,_,files in os.walk(root_path):
             for f in files:
@@ -34,3 +34,18 @@ if __name__ == "__main__":
                     img = Image.open(fn)
                     tags = get_tags(img)
                     fw.write(",".join(tags)+"\n")
+
+#batch_tag(get_tags, root_path, output_fn)
+
+def test():
+    image = Image.open("D:/datasets/1.jpg")
+    sorted_keys = get_tags(image)
+    print(sorted_keys)
+
+if __name__ == "__main__":
+    
+    app =  gr.Interface(fn=get_tags_str,inputs=[gr.Image(type="pil"),gr.Slider(0,1,0.5)],outputs=["text"])
+    app.launch(
+       server_name = "0.0.0.0" ,
+       server_port = 8275,
+    )
