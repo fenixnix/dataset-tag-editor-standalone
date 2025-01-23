306de81ef85385fad20498d6c1eb059edc2a0eb9
Nibo <57319138@qq.com>
2025-01-23 12:12:32 +0800

    Update nibo_webapp.py

--- scripts/nibo_webapp.py
+++ scripts/nibo_webapp.py
@@ -5,11 +5,15 @@ from PIL import Image
 
 from dataset_tag_editor.tagger import WaifuDiffusion
 
-
-tag = WaifuDiffusion("wd-v1-4-convnext-tagger-v2",0.7)
+tag = WaifuDiffusion("wd-v1-4-convnext-tagger-v2",0.5)
 tag.start()
 
-image = Image.open("D:/datasets/1.jpg")
+def get_tags(image,thresh:float = 0.5):
+    output = tag.predict(image,thresh)
+    sorted_items = sorted(output.items(), key=lambda item: item[1],reverse=True)
+    sorted_keys = [item[0] for item in sorted_items]
+    return sorted_keys
 
-output = tag.predict(image,0.7)
-print(output)
+image = Image.open("D:/datasets/1.jpg")
+sorted_keys = get_tags(image)
+print(sorted_keys)
