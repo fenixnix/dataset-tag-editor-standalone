4eab8e644bee3a50e529c13bcd468c062c65d08c
Nibo <57319138@qq.com>
2025-01-23 12:29:54 +0800

    Update nibo_webapp.py

--- scripts/nibo_webapp.py
+++ scripts/nibo_webapp.py
@@ -1,6 +1,8 @@
 import os
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 
+from pathlib import Path
+
 from PIL import Image
 
 from dataset_tag_editor.tagger import WaifuDiffusion
@@ -17,3 +19,18 @@ def get_tags(image,thresh:float = 0.5):
 image = Image.open("D:/datasets/1.jpg")
 sorted_keys = get_tags(image)
 print(sorted_keys)
+
+if __name__ == "__main__":
+    root_path = "Z:/comic_database/comic_grid/21世紀小福星"
+    output_fn = "Z:/comic_database/comic_grid/21世紀小福星/tags.txt"
+    with open(output_fn,'w',encoding="utf-8") as fw:
+        for p,_,files in os.walk(root_path):
+            for f in files:
+                file_path = Path(f)
+                if file_path.suffix in [".png",".jpg",".jpeg",".webp"]:
+                    fn = os.path.join(p,f)
+                    print(fn)
+                    fw.write(fn+"\n")
+                    img = Image.open(fn)
+                    tags = get_tags(img)
+                    fw.write(",".join(tags)+"\n")
